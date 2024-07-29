46de207ae630a7bd31a937057d97d1a0f1ca8133
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2024-07-29 14:37:03 +0400

    updated +

--- app.js
+++ app.js
@@ -955,9 +955,6 @@ app.get('/ratings/compare', passport.authenticate('jwt', { session: false }), as
 });
 
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
